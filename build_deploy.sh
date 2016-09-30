940396dc97441adc9d260f9ac8a8f7cfe36f786f
Darren Reid <darren.j.reid@gmail.com>
2016-09-30 11:25:18 +1000

    Init ecs service if doesn't exist

--- build_deploy.sh
+++ build_deploy.sh
@@ -12,7 +12,8 @@ ECS_SERVICE=netcoreapps-chat-service
 ECS_TASK=netcoreapps-chat-task
 AWS_DEFAULT_REGION=ap-southeast-2
 AWS_ECS_CLUSTER_NAME=default
-#AWS_ECS_REPO_DOMAIN={} set in private variable
+#AWS_ACCOUNT_NUMBER={} set in private variable
+AWS_ECS_REPO_DOMAIN=$AWS_ACCOUNT_NUMBER.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 
 # Build/Deploy process
 eval $(aws ecr get-login --region $AWS_DEFAULT_REGION) #needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
@@ -21,6 +22,12 @@ docker tag $IMAGE_NAME $AWS_ECS_REPO_DOMAIN/$IMAGE_NAME:$IMAGE_VERSION
 docker push $AWS_ECS_REPO_DOMAIN/$IMAGE_NAME:$IMAGE_VERSION
 aws ecs register-task-definition --cli-input-json file://task-definition.json  --region $AWS_DEFAULT_REGION > /dev/null # Create a new task revision
 TASK_REVISION=$(aws ecs describe-task-definition --task-definition $ECS_TASK  --region $AWS_DEFAULT_REGION | jq '.taskDefinition.revision') #get latest revision
+if [ aws ecs list-services --cluster $AWS_ECS_CLUSTER_NAME | jq '.serviceArns' | jq 'contains("arn:aws:ecs:$AWS_DEFAULT_REGION:$AWS_ACCOUNT_NUMBER:service/$ECS_SERVICE")']; then
+    echo "ECS Service already exists"
+else
+    echo "Creating ECS Service $ECS_SERVICE"
+    aws ecs create-service --service-name $ECS_SERVICE --task-definition $ECS_TASK:$TASK_REVISION --desired-count 1
+fi
 aws ecs update-service --cluster $AWS_ECS_CLUSTER_NAME --service $ECS_SERVICE --task-definition "$ECS_TASK:$TASK_REVISION"  --region $AWS_DEFAULT_REGION > /dev/null #update service with latest task revision
 TEMP_ARN=$(aws ecs list-tasks --service-name $ECS_SERVICE  --region $AWS_DEFAULT_REGION | jq '.taskArns[0]') # Get current running task ARN
 TASK_ARN="${TEMP_ARN%\"}" # strip double quotes
