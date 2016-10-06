1d08d650e905cc37328705aaaefaba91660c468c
Darren Reid <darren.j.reid@gmail.com>
2016-10-06 14:11:51 +1100

    Update build deploy process

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
+source ./set-envs.sh
+
+# Update task definition with env values
+sed "s/__ECS_TASK__/$ECS_TASK/g" -i ./task-definition.json
+sed "s/__IMAGE_NAME__/$IMAGE_NAME/g" -i ./task-definition.json
+sed "s/__AWS_ECS_REPO_DOMAIN__/$AWS_ECS_REPO_DOMAIN/g" -i ./task-definition.json
+sed "s/__IMAGE_VERSION__/$IMAGE_VERSION/g" -i ./task-definition.json
 
 # install dependencies
 sudo apt-get install jq #install jq for json parsing
 pip install --user awscli # install aws cli w/o sudo
 export PATH=$PATH:$HOME/.local/bin # put aws in the path
 
-# Set variables
-IMAGE_NAME=netcoreapps
-IMAGE_VERSION=latest
-ECS_SERVICE=netcoreapps-chat-service
-ECS_TASK=netcoreapps-chat-task
-AWS_DEFAULT_REGION=ap-southeast-2
-AWS_ECS_CLUSTER_NAME=default
-#AWS_ACCOUNT_NUMBER={} set in private variable
-AWS_ECS_REPO_DOMAIN=$AWS_ACCOUNT_NUMBER.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
-
-# Build/Deploy process
 eval $(aws ecr get-login --region $AWS_DEFAULT_REGION) #needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
-docker build -t $IMAGE_NAME .
-docker tag $IMAGE_NAME $AWS_ECS_REPO_DOMAIN/$IMAGE_NAME:$IMAGE_VERSION
 docker push $AWS_ECS_REPO_DOMAIN/$IMAGE_NAME:$IMAGE_VERSION
 aws ecs register-task-definition --cli-input-json file://task-definition.json --region $AWS_DEFAULT_REGION > /dev/null # Create a new task revision
 TASK_REVISION=$(aws ecs describe-task-definition --task-definition $ECS_TASK --region $AWS_DEFAULT_REGION | jq '.taskDefinition.revision') #get latest revision
-CREATE_NEW_SERVICE=$(aws ecs list-services --region ap-southeast-2 --cluster $AWS_ECS_CLUSTER_NAME | jq '.serviceArns' | jq 'contains(["arn:aws:ecs:$AWS_DEFAULT_REGION:$AWS_ACCOUNT_NUMBER:service/$ECS_SERVICE"])')
+CREATE_NEW_SERVICE=$(aws ecs list-services --region $AWS_DEFAULT_REGION --cluster $AWS_ECS_CLUSTER_NAME | jq '.serviceArns' | jq 'contains(["arn:aws:ecs:$AWS_DEFAULT_REGION:$AWS_ACCOUNT_NUMBER:service/$ECS_SERVICE"])')
 if [ "$CREATE_NEW_SERVICE" = "true" ]; then
     echo "ECS Service already exists"
     aws ecs update-service --cluster $AWS_ECS_CLUSTER_NAME --service $ECS_SERVICE --task-definition "$ECS_TASK:$TASK_REVISION" --desired-count 1 --region $AWS_DEFAULT_REGION > /dev/null #update service with latest task revision
