ae6c509d50875a4c10813ba24be99cedbdbf9b13
Darren Reid <darren.j.reid@gmail.com>
2016-10-13 13:51:47 +1100

    Simplify build/deploy scripts

---
+++ deploy-envs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Set variables
+export IMAGE_NAME=netcoreapps-chat
+export IMAGE_VERSION=latest
+export AWS_DEFAULT_REGION=us-east-1
+export AWS_ECS_CLUSTER_NAME=default
+
+export AWS_VIRTUAL_HOST=chat.netcore.io
+export AWS_ECS_HOST_PORT=5000
